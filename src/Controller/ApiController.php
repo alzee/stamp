ff1c61755b235bb54f1daf32682a58509afa73f5
Al Zee <z@alz.ee>
2022-04-20 18:39:32 +0800

    /api/wecom

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,11 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\wecom\callback\WXBizMsgCrypt;
 
+#[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
+    #[Route('/', name: 'app_api')]
     public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+
+    #[Route('/wecom', name: 'app_wecom')]
+    public function wecom(): Response
     {
         return $this->render('api/index.html.twig', [
             'controller_name' => 'ApiController',
