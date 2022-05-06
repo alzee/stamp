7237634788cf865217573da42982c3d5d90a7ac2
Al Zee <z@alz.ee>
2022-05-06 20:30:13 +0800

    symfony/orm-pack

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
