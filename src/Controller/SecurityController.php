a93b829a37983c35f3ceafbae4718f38965801cb
Al Zee <z@alz.ee>
2024-01-17 10:22:55 +0800

    nth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,7 +21,13 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+          'last_username' => $lastUsername,
+          'error' => $error,
+          // 'page_title' => $conf->getSiteName(),
+          'csrf_token_intention' => 'authenticate',
+          'target_path' => $this->generateUrl('admin'),
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
